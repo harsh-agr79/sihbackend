18241c18c6b6b94c3df6c45cc95ad7267d67811d
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-23 00:12:14 +0530

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Password;
-use App\Notifications\VerifyEmailNotification;
+
 
 class AuthController extends Controller {
     
--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -7,6 +7,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Notifications\ResetPasswordNotification;
+use App\Notifications\VerifyEmailNotification;
 
 class Mentor extends Authenticatable implements MustVerifyEmail
 {
