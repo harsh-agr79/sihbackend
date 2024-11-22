49868379d00b952dfb692f6e4e3482c33269b22d
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-23 00:36:37 +0530

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller {
 
