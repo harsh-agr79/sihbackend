7e26bfb2e29b563d568c7c6729bf9fcbffaf299e
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-26 00:48:59 +0530

    changes

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -75,7 +75,7 @@ class Course extends Model
 
     public function ungroupedModules()
     {
-        return $this->modules()->whereNull('group_id')->get();
+        return $this->hasMany(Module::class)->whereNull('group_id');
     }
 }
 
