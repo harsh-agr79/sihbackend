4d9048c1d85137e1aa8191d2f08895e46af33cf0
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-26 15:55:16 +0530

    submit assignment

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -756,7 +756,7 @@ class CourseController extends Controller {
     {
         // Validate the request
         $validated = $request->validate([
-            'assignment_id' => 'required|exists:assignments_quizzes,id',
+            'assignment_id' => 'required|exists:assignment_quizzes,id',
             'submission_content' => 'required|array', // Ensure content is an array
             'submission_content.answers' => 'required|array', // Ensure answers array exists
             'submission_content.answers.*.id' => 'required|integer', // Validate question IDs
