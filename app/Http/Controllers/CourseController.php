f02b430788b0c6c1234af4a1314579baeec29076
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-26 16:27:25 +0530

    submit assignment

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -830,7 +830,6 @@ class CourseController extends Controller {
     }
     public function editSubmission(Request $request)
     {
-        return response()->json([ 'data'=>$request->post()]);
 
         $validated = $request->validate([
             'submission_id' => 'required|exists:submissions,id',
