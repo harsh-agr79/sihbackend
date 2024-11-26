e4d00e26c92a77c6e2cf241652c6aade8386221f
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-26 16:20:33 +0530

    submit assignment

--- routes/api.php
+++ routes/api.php
@@ -43,8 +43,8 @@ Route::group(['middleware'=>'api_key'], function () {
             Route::get('/getcourses', [CourseController::class, 'getCourses']);
             Route::get('/courses/details/{courseId}', [CourseController::class, 'getCourseDetails']);
             Route::post('/courses/enroll', [CourseController::class, 'enrollStudent']);
-            ROute::post('/course/assignment/submit', [CourseController::class, 'submitAssignment']);
-            ROute::put('/course/assignment/editsubmit', [CourseController::class, 'editSubmission']);
+            Route::post('/course/assignment/submit', [CourseController::class, 'submitAssignment']);
+            Route::put('/course/assignment/editsubmit', [CourseController::class, 'editSubmission']);
         });
     });
     
