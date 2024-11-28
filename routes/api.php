0e8bed28a13f80e831a9f08242246a442ce97a5b
harsh-agr79 <agrharsh7932@gmail.com>
2024-11-28 22:56:32 +0530

    chnage

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,7 @@ Route::group(['middleware'=>'api_key'], function () {
 
             Route::get('/getcourses', [CourseController::class, 'getCourses']);
             Route::get('/courses/details/{courseId}', [CourseController::class, 'getCourseDetails']);
+            Route::get('/mycourses/details/{courseId}', [CourseController::class, 'getMyCourseDetails']);
             Route::get('/mycourses', [CourseController::class, 'getStudentEnrolledCourses']);
             Route::post('/courses/enroll', [CourseController::class, 'enrollStudent']);
             Route::post('/course/assignment/submit', [CourseController::class, 'submitAssignment']);
